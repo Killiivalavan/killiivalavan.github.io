04ae094c8e34687fde9d3ec10df8bde7fcb860ad
Killiivalavan <kavalavan04@gmail.com>
2025-06-05 16:52:09 +0530

    deployment1

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   distDir: 'out',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-website' : '',
   images: {
     unoptimized: true,
     domains: [
